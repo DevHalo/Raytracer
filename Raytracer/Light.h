9a7b98187e183fcde88ddc7acf46c3f67bd4579e
Mark Voong <devhalo@hotmail.com>
2018-11-22 17:10:03 -0500

    Changed to homogeneous coordinates

--- Raytracer/Light.h
+++ Raytracer/Light.h
@@ -1,11 +1,11 @@
 #pragma once
 #include <string>
-#include "include/glm/vec3.hpp"
+#include "include/glm/vec4.hpp"
 
 // Container for a light element in the scene
 struct Light
 {
 	std::string name;		// Name of the object
-	glm::vec3 pos;			// Position of the object in 3D space
-	glm::vec3 intensity;	// RGB values of the light's intensity
+	glm::vec4 pos;			// Position of the object in 3D space
+	glm::vec4 intensity;	// RGB values of the light's intensity
 };
--- Raytracer/Sphere.h
+++ Raytracer/Sphere.h
@@ -1,13 +1,14 @@
 #pragma once
 #include "include/glm/vec3.hpp"
+#include "include/glm/vec4.hpp"
 #include <string>
 
 // Container for a sphere object in the scene
 struct Sphere
 {
 	std::string name;					// Name of the object
-	glm::vec3 pos;						// Position of sphere in 3D space
-	glm::vec3 scl;						// Scalar size of sphere
+	glm::vec4 pos;						// Position of sphere in 3D space
+	glm::vec4 scl;						// Scalar size of sphere
 	glm::vec3 colour;					// RGB values of sphere
 	float k_a, k_diff, k_spec, k_refl;	// Illumination model variables
 	int n;								// Specular Component
