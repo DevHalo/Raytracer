4fd1816f548c1c92630595900d20ed310c755908
Mark Voong <devhalo@hotmail.com>
2018-11-23 12:15:37 -0500

    Raytracing unfinished

--- Raytracer/Ray.cpp
+++ Raytracer/Ray.cpp
@@ -1,27 +1,26 @@
 #include "pch.h"
 #include "Ray.h"
 #include "FileReader.h"
-#include <iostream>
 #include "glm/glm.hpp"
 
-glm::vec4 s = { 0.0f, 0.0f, 0.0f, 1.0f };	// Ray starting point, or eye
-glm::vec4 c = { 0.0f, 0.0f, 0.0f, 1.0f };	// Direction of ray, intersects with near plane
+glm::vec4 o = { 0.0f, 0.0f, 0.0f, 1.0f };	// Ray starting point, or eye
+glm::vec4 d = { 0.0f, 0.0f, 0.0f, 1.0f };	// Direction of ray, intersects with near plane
 float t = FLT_MAX;							// Scalar for direction
 
-// r(t) = a + ct, t = {0, +inf}
+// r(t) = o + dt, t = {0, +inf}
 
 int depth = 0;
 
 // Casts a ray with supplied origin and direction
 Ray::Ray(glm::vec4 origin, glm::vec4 direction)
 {
-	s.x = origin.x;
-	s.y = origin.y;
-	s.z = origin.z;
+	o.x = origin.x;
+	o.y = origin.y;
+	o.z = origin.z;
 
-	c.x = direction.x;
-	c.y = direction.y;
-	c.z = direction.z;
+	d.x = direction.x;
+	d.y = direction.y;
+	d.z = direction.z;
 }
 
 // Casts a ray from the camera origin to the column and pixel location
@@ -29,9 +28,9 @@ Ray::Ray(std::unique_ptr<Parameters>& params, float width, float height, int col
 {
 	// Formula is eye + t(P(r,c) - eye) but eye is (0, 0, 0) therefore only P(r,c)
 	// needs to be defined
-	c.x = width * ((2.0f * col / *params->resX) - 1.0f);
-	c.y = height * ((2.0f * row / *params->resY) - 1.0f);
-	c.z = -*params->near;
+	o.x = width * ((2.0f * col / *params->resX) - 1.0f);
+	o.y = height * ((2.0f * row / *params->resY) - 1.0f);
+	o.z = -*params->near;
 
 	depth = 0;
 }
@@ -46,28 +45,28 @@ std::shared_ptr<Sphere> Ray::SphereIntersect(std::vector<std::shared_ptr<Sphere>
 		// If the sphere is canonical, we do not need to do any transformations
 		if ((*it)->scl.x == 1 && (*it)->scl.y == 1 && (*it)->scl.z == 1)
 		{
+			// Formula:
+			// |d|^2t + 2(d . c)t + |c|^2 - 1 = 0 
+
+			glm::vec4 c = o - (*it)->pos;
+
 			// Get each component in at^2 + 2bt + c = 0
-			//glm::vec4 S = a - (*it)->pos;	// Origin of eye minus origin of circle
-			float a = dot(c, c);	// 
-			float b = 2 * dot(c, s);
-			float c = dot(s, s) - 1;
+			float qA = dot(d, d);
+			float qB = 2 * dot(d, c);
+			float qC = dot(c, c) - 1;
 
-			// Find Discriminant
-			float discriminant = b * b - a * c;
-			if (discriminant == 0)
-			{
-				tempT = -b / a;
-				closestSphere = *it;
+			float discriminant = (qB * qB) - (4 * qA * qC);
 
-				std::cout << "COLLISION: " << (*it)->name << " AT: " << tempT;
-			}
-			else if (discriminant > 0)
+			if (discriminant > 0)
 			{
-				tempT = (-b + discriminant) / a;
-				float tempT2 = (-b - discriminant) / a;
-				closestSphere = *it;
 				
-				std::cout << "COLLISION: " << (*it)->name << " AT: " << tempT << ", " << tempT2;
+			}
+			else if (discriminant == 0)
+			{
+				
+			}
+			else
+			{
 				
 			}
 		}
