88e5fa643ecf1ddbc52c672f1f68f134d86b2aac
Mark Voong <devhalo@hotmail.com>
2018-11-23 17:12:03 -0500

    Raytracer collision working

--- Raytracer/Ray.cpp
+++ Raytracer/Ray.cpp
@@ -4,15 +4,40 @@
 #include "glm/glm.hpp"
 
 glm::vec4 o = { 0.0f, 0.0f, 0.0f, 1.0f };	// Ray starting point, or eye
-glm::vec4 d = { 0.0f, 0.0f, 0.0f, 1.0f };	// Direction of ray, intersects with near plane
+glm::vec4 d = { 0.0f, 0.0f, 0.0f, 0.0f };	// Direction of ray, intersects with near plane
 float t = FLT_MAX;							// Scalar for direction
 
 // r(t) = o + dt, t = {0, +inf}
 
 int depth = 0;
 
+// Returns the depth of the current ray being traced
+int Ray::Depth() { return depth; }
+
+// Returns the origin point of the ray
+glm::vec4 Ray::Origin() const { return o; }
+
+// Returns the non-normalized direction of the ray
+glm::vec4 Ray::Direction() const { return d; }
+
+// Sets a new direction vector for the ray
+void Ray::SetDirection(glm::vec4 newDirection) { d = newDirection; }
+
+// Clamps a colour uniformly
+void Ray::ClampColour(glm::vec3 colour)
+{
+	float maxColour = colour.r;
+
+	// Determine largest colour value from r,g,b
+	if (maxColour < colour.g) { maxColour = colour.g; }
+	if (maxColour < colour.b) { maxColour = colour.b; }
+
+	// Clamp by scaling each colour by the largest
+	if (maxColour > 1.0f) { colour.r /= maxColour; colour.g /= maxColour; colour.b /= maxColour; }
+}
+
 // Casts a ray with supplied origin and direction
-Ray::Ray(glm::vec4 origin, glm::vec4 direction)
+Ray::Ray(glm::vec4 origin, glm::vec4 direction, int _depth)
 {
 	o.x = origin.x;
 	o.y = origin.y;
@@ -21,6 +46,8 @@ Ray::Ray(glm::vec4 origin, glm::vec4 direction)
 	d.x = direction.x;
 	d.y = direction.y;
 	d.z = direction.z;
+
+	depth = _depth;
 }
 
 // Casts a ray from the camera origin to the column and pixel location
@@ -28,53 +55,107 @@ Ray::Ray(std::unique_ptr<Parameters>& params, float width, float height, int col
 {
 	// Formula is eye + t(P(r,c) - eye) but eye is (0, 0, 0) therefore only P(r,c)
 	// needs to be defined
-	o.x = width * ((2.0f * col / *params->resX) - 1.0f);
-	o.y = height * ((2.0f * row / *params->resY) - 1.0f);
-	o.z = -*params->near;
+	d.x = width * ((2.0f * col / *params->resX) - 1.0f);
+	d.y = height * ((2.0f * row / *params->resY) - 1.0f);
+	d.z = -*params->near;
 
 	depth = 0;
 }
 
-// Attempts to find a collision point with
-std::shared_ptr<Sphere> Ray::SphereIntersect(std::vector<std::shared_ptr<Sphere>>& spheres) const
+// Returns a struct containing all components of the quadratic as well as
+// the intercepts and whether or not to flip the normal (If the collision point is inside an object)
+// Returns nullptr if no collision is found
+std::unique_ptr<Quadratic> Ray::GetCollisionPoint(std::shared_ptr<Sphere> sphere)
 {
-	float tempT;
-	std::shared_ptr<Sphere> closestSphere = nullptr;
-	for (std::vector<std::shared_ptr<Sphere>>::iterator it = spheres.begin(); it != spheres.end(); ++it)
+	glm::vec4 invDir = d;
+	
+	// If the sphere is non-canonical, we must transform the ray relative to the sphere
+	if (sphere->scl.x != 1 || sphere->scl.y != 1 || sphere->scl.z != 1) { invDir /= (sphere->scl * sphere->scl); }
+
+	// Formula:
+	// |d|^2t + 2(d . c)t + |c|^2 - 1 = 0 
+
+	glm::vec4 c = o - sphere->pos;
+
+	// Get each component in At^2 + 2Bt + C = 0
+	//float A = dot(invDir, invDir);
+	//float B = 2 * dot(invDir, c);
+	//float C = dot(c, c) - 1;
+
+	float A =
+		((d.x * d.x) / (sphere->scl.x * sphere->scl.x)) +
+		((d.y * d.y) / (sphere->scl.y * sphere->scl.y)) +
+		((d.z * d.z) / (sphere->scl.z * sphere->scl.z));
+
+	float B =
+		((2.f * d.x * sphere->pos.x) / (sphere->scl.x * sphere->scl.x)) +
+		((2.f * d.y * sphere->pos.y) / (sphere->scl.y * sphere->scl.y)) +
+		((2.f * d.z * sphere->pos.z) / (sphere->scl.z * sphere->scl.z));
+
+	float C =
+		((sphere->pos.x * sphere->pos.x) / (sphere->scl.x * sphere->scl.x)) +
+		((sphere->pos.y * sphere->pos.y) / (sphere->scl.y * sphere->scl.y)) +
+		((sphere->pos.z * sphere->pos.z) / (sphere->scl.z * sphere->scl.z));
+
+	float discriminant = (B * B) - (4 * A * C);
+	Quadratic q;
+	std::unique_ptr<Quadratic> qPtr;
+
+	if (discriminant > 0)
 	{
-		// If the sphere is canonical, we do not need to do any transformations
-		if ((*it)->scl.x == 1 && (*it)->scl.y == 1 && (*it)->scl.z == 1)
+		discriminant = sqrt(discriminant);
+
+		// We need to compare two values
+		float t1 = (-B + discriminant) / A;
+		float t2 = (-B - discriminant) / A;
+
+		q = { A, B, C, t1, t2, sphere };
+		qPtr = std::make_unique<Quadratic>(q);
+	}
+
+	if (discriminant == 0)
+	{
+		discriminant = sqrt(discriminant);
+
+		// Doesn't matter which value we compute, compare with the shortest distance t
+		float t1 = (-B + discriminant) / A;
+
+		q = { A, B, C, t1, t1, sphere };
+		qPtr = std::make_unique<Quadratic>(q);
+	}
+
+	// No collision, return nullptr
+	return qPtr;
+}
+
+// Returns a struct containing all the components of a quadratic of the
+// closest intersection between a set of objects
+// Returns nullptr if no collision is found
+std::unique_ptr<Quadratic> Ray::ShortestCollisionPoint(std::vector<std::shared_ptr<Sphere>> spheres)
+{
+	std::unique_ptr<Quadratic> quad;
+	for (auto it = spheres.begin(); it != spheres.end(); ++it)
+	{
+		std::unique_ptr<Quadratic> tempQ = GetCollisionPoint(*it);
+
+		// If there is an actual collision
+		if (tempQ)
 		{
-			// Formula:
-			// |d|^2t + 2(d . c)t + |c|^2 - 1 = 0 
-
-			glm::vec4 c = o - (*it)->pos;
-
-			// Get each component in at^2 + 2bt + c = 0
-			float qA = dot(d, d);
-			float qB = 2 * dot(d, c);
-			float qC = dot(c, c) - 1;
-
-			float discriminant = (qB * qB) - (4 * qA * qC);
-
-			if (discriminant > 0)
+			if (!quad)
 			{
-				
-			}
-			else if (discriminant == 0)
-			{
-				
+				// First collision that has occurred
+				quad = std::move(tempQ);
 			}
 			else
 			{
-				
+				// If the new collision is closer to the ray, we reuse this instead
+				if (tempQ < quad)
+				{
+					quad = std::move(tempQ);
+				}
 			}
-		}
-		else
-		{
-			// Ellipsoid, transform matrix to get inverse ray
 		}
 	}
 
-	return closestSphere;
+	return quad;
 }
--- Raytracer/Ray.h
+++ Raytracer/Ray.h
@@ -1,11 +1,37 @@
 #pragma once
 #include "FileReader.h"
 
+// Data structure to store the quadratic equation formed from a collision
+struct Quadratic
+{
+	float A;
+	float B;
+	float C;
+	float t1;
+	float t2;
+	std::shared_ptr<Sphere> sphere;
+
+	bool operator<(Quadratic other) const
+	{
+		float minT1 = t1 < t2 ? t1 : t2;
+		float minT2 = other.t1 < other.t2 ? t1 : t2;
+
+		return minT1 < minT2;
+	}
+};
+
 class Ray
 {
 public:
-	Ray(glm::vec4 origin, glm::vec4 direction);
+	int Depth();
+	static void ClampColour(glm::vec3 colour);
+	Ray(glm::vec4 origin, glm::vec4 direction, int _depth);
 	Ray(std::unique_ptr<Parameters>& params, float width, float height, int col, int row);
-	std::shared_ptr<Sphere> SphereIntersect(std::vector<std::shared_ptr<Sphere>>& spheres) const;
+	std::unique_ptr<Quadratic> ShortestCollisionPoint(std::vector<std::shared_ptr<Sphere>> spheres);
+	glm::vec4 Origin() const;
+	glm::vec4 Direction() const;
+	void SetDirection(glm::vec4 newDirection);
+private:
+	std::unique_ptr<Quadratic> GetCollisionPoint(std::shared_ptr<Sphere> sphere);
 };
 
