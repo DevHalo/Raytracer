5918c9cbc690ad19f47e84e7ab1733cb46cff930
Mark Voong <devhalo@hotmail.com>
2019-01-11 03:07:13 -0500

    Optimization of raytracing function

--- Raytracer/Ray.h
+++ Raytracer/Ray.h
@@ -18,14 +18,14 @@ class Ray
 public:
 	int Depth() const;
 	static void ClampColour(glm::vec3& colour);
-	Ray(glm::vec4 origin, glm::vec4 direction, int _depth);
+	Ray(glm::vec4& origin, glm::vec4& direction, int _depth);
 	Ray(std::unique_ptr<Parameters>& params, float width, float height, int col, int row);
-	std::unique_ptr<Quadratic> ShortestCollisionPoint(std::vector<std::shared_ptr<Sphere>> spheres);
-	bool Collides(std::vector<std::shared_ptr<Sphere>> spheres);
+	std::unique_ptr<Quadratic> ShortestCollisionPoint(std::vector<std::shared_ptr<Sphere>>& spheres);
+	bool Collides(std::vector<std::shared_ptr<Sphere>>& spheres);
 	glm::vec4 Origin() const;
 	glm::vec4 Direction() const;
 private:
-	std::unique_ptr<Quadratic> GetCollisionPoint(std::shared_ptr<Sphere> sphere);
+	std::unique_ptr<Quadratic> GetCollisionPoint(std::shared_ptr<Sphere>& sphere);
 	glm::vec4 o = { 0.f, 0.f, 0.f, 1.f };	// Ray starting point, or eye
 	glm::vec4 d = { 0.f, 0.f, 0.f, 0.f };	// Direction of ray, intersects with near plane
 	float t = FLT_MAX;						// Scalar for direction
